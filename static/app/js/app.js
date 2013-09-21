1241d5aacc2a53a7924e73113033319cf8c11dee
Kenny Katzgrau <katzgrau@gmail.com>
2013-09-21 17:48:30 -0400

    Half-baked end of competition commit

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -1,25 +1,67 @@
+/* Not supposed to use jQuery in a controller, but we've only
+   got four minutes to save the world
+*/
 
 function MainCtrl($scope) {
 
-    $scope.main = {};
+    $scope.years = [];
+    $scope.year  = null;
 
-    $scope.main.years = [2013, 2012, 2011, 2010];
-    $scope.main.year  = $scope.main.years[0];
+    $scope.districts = [];
+    $scope.district  = null;
 
+    $scope.seats = [];
+    $scope.seat  = null;
 
-    $scope.main.showRaces = false;
-    $scope.main.races     = [{id: 0, name: 'Race #1'}];
-    $scope.main.race      = $scope.main.races[0];
+    $scope.showRaces = false;
+    $scope.races     = [{id: 0, name: 'Race #1'}];
+    $scope.race      = $scope.races[0];
 
-    $scope.main.showCandidates = false;
-    $scope.main.candidates     = [{id: 0, name: 'Mitt Romney'}];
-    $scope.main.candidate      = $scope.main.candidates[0];
+    $scope.showCandidates = false;
+    $scope.candidates     = [{id: 0, name: 'Mitt Romney'}];
+    $scope.candidate      = $scope.candidates[0];
 
-    $scope.showData = false;
+    $scope.showData  = false;
 
     $scope.init = function() {
-        $scope.main.title = 'Campaign Contribution by Industry';
-        $scope.main.help = 'Pick a Year';
+        $scope.title = 'Campaign Contribution by Industry';
+        $scope.help  = 'Pick a Year';
+
+        $scope.get('/years', function(years) {
+          for(var i in years)
+                $scope.years.push(years[i]);
+          $scope.year = $scope.years[0];
+
+        });
+    }
+
+    $scope.loadDistricts = function() {
+        $scope.get('/years/' + $scope.year, function(districts) {
+          for(var i in districts)
+                $scope.districts.push(districts[i]);
+
+          $scope.district = $scope.districts[0];
+        });
+    }
+
+    $scope.loadSeats = function() {
+        $scope.get('/years/' + $scope.year + '/' + $scope.district, function(seats) {
+          for(var i in seats)
+                $scope.seats.push(seats[i]);
+
+          $scope.seat = $scope.seats[0];
+        });
+    }
+
+    $scope.get = function(uri, func) {
+        $.ajax({
+            type: 'GET',
+            url: uri,
+            dataType: 'json',
+            success: func,
+            data: {},
+            async: false
+        });
     }
 
     $scope.init();
