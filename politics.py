c8eeffcdb4e5ad7cfa6f498b24d7a73200be37ca
John Crepezzi <john.crepezzi@gmail.com>
2013-09-21 13:25:06 -0400

    woo

--- politics.py
+++ politics.py
@@ -2,29 +2,21 @@ import web
 import json
 import sunlight
 
-# This is our router
-urls = (
+app = web.application((
     '/hi',          'SayHi',
     '/legislators', 'Legislators'
-)
+), globals())
 
-# create a new application
-app = web.application(urls, globals())
-
-# Say hi to the world!
 class SayHi:
-
-    # We'll say hi to anyone.
+    'Say hi to the world!'
     def GET(self):
         return 'Hey Hack Jersey!'
 
 class Legislators:
-
-    # List some legislators so we can prove we're pulling data
+    'List some legislators showing pulling data from the API'
     def GET(self):
         web.header('Content-Type', 'application/json')
         return json.dumps(sunlight.openstates.legislators(state="nj"))
 
-# Run the application
 if __name__ == '__main__':
     app.run()
