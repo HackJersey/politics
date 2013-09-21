1994fe37729caf504c55b344cfe3a220065173de
Kenny Katzgrau <katzgrau@gmail.com>
2013-09-21 16:22:46 -0400

    Added controls for drilling down w/ fake data

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -7,6 +7,16 @@ function MainCtrl($scope) {
     $scope.main.year  = $scope.main.years[0];
 
 
+    $scope.main.showRaces = false;
+    $scope.main.races     = [{id: 0, name: 'Race #1'}];
+    $scope.main.race      = $scope.main.races[0];
+
+    $scope.main.showCandidates = false;
+    $scope.main.candidates     = [{id: 0, name: 'Mitt Romney'}];
+    $scope.main.candidate      = $scope.main.candidates[0];
+
+    $scope.showData = false;
+
     $scope.init = function() {
         $scope.main.title = 'Campaign Contribution by Industry';
         $scope.main.help = 'Pick a Year';
