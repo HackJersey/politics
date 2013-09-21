673c49aea219a335d25fb14356614555d91e8dcd
John Crepezzi <john.crepezzi@gmail.com>
2013-09-21 13:22:47 -0400

    Integrate something with sunlight

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ To run the application on `localhost:8080`:
 
 1.  Install Python
 2.  Install web.py: `easy_install web.py`
-3.  Start the application: `python politics.py`
+3.  install sunlight: `easy_install sunlight`
+4.  Create a file containing your API key at `~/.sunlight.key`
+5.  Start the application: `python politics.py`
--- politics.py
+++ politics.py
@@ -1,8 +1,11 @@
 import web
+import json
+import sunlight
 
 # This is our router
 urls = (
-    '/hi', 'politics.SayHi'
+    '/hi',          'SayHi',
+    '/legislators', 'Legislators'
 )
 
 # create a new application
@@ -14,6 +17,13 @@ class SayHi:
     # We'll say hi to anyone.
     def GET(self):
         return 'Hey Hack Jersey!'
+
+class Legislators:
+
+    # List some legislators so we can prove we're pulling data
+    def GET(self):
+        web.header('Content-Type', 'application/json')
+        return json.dumps(sunlight.openstates.legislators(state="nj"))
 
 # Run the application
 if __name__ == '__main__':
