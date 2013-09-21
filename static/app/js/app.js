1e6ad3f56526c54d83df6c81e4434d3e3a75c534
Kenny Katzgrau <katzgrau@gmail.com>
2013-09-21 16:09:13 -0400

    Added basic select box for year

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -3,6 +3,9 @@ function MainCtrl($scope) {
 
     $scope.main = {};
 
+    $scope.main.years = [2013, 2012, 2011, 2010];
+    $scope.main.year  = $scope.main.years[0];
+
 
     $scope.init = function() {
         $scope.main.title = 'Campaign Contribution by Industry';
