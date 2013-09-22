1ca1473cc869454039c9620459f2233d1bb168e1
Kenny Katzgrau <katzgrau@gmail.com>
2013-09-22 11:47:54 -0400

    Asynchronous calls

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -1,21 +1,25 @@
+var hackjersey = angular.module('hackjersey', []);
+
 /* Not supposed to use jQuery in a controller, but we've only
  * got four minutes to save the world
  *
  * @broadstreetads
 */
-function MainCtrl($scope) {
+hackjersey.controller('MainCtrl', function($scope, $http) {
 
-    $scope.years = [];
-    $scope.year  = null;
+    $scope.candidates = [];
+    $scope.candidate  = $scope.candidates[0];
+
+    $scope.charts = []; // Will hold data for charts
 
     $scope.districts = [];
     $scope.district  = null;
 
     $scope.seats = [];
     $scope.seat  = null;
-
-    $scope.candidates     = [];
-    $scope.candidate      = $scope.candidates[0];
+    
+    $scope.years = [];
+    $scope.year  = null;
 
     $scope.showData  = true;
 
@@ -25,45 +29,63 @@ function MainCtrl($scope) {
     $scope.init = function() {
         $scope.title = 'Campaign Contribution by Industry';
         $scope.help  = 'Pick a Year';
-        $scope.loadItems('/years', 'years');
-        $scope.year  = $scope.years[0];
-        $scope.loadDistricts();
+        $scope.loadItems('/years', 'years', function() {
+            $scope.year  = $scope.years[0];
+            $scope.loadDistricts();
+        });
     }
 
     /**
      * Load districts
      */
     $scope.loadDistricts = function() {
-        $scope.loadItems('/years/' + $scope.year, 'districts');
-        $scope.district = $scope.districts[0];
-        $scope.loadSeats();
+        $scope.loadItems('/years/' + $scope.year, 'districts', function() {
+            $scope.district = $scope.districts[0];
+            $scope.loadSeats();
+        });
     }
 
     /**
      * Load seats
      */
     $scope.loadSeats = function() {
-        $scope.loadItems('/years/' + $scope.year + '/' + $scope.district, 'seats');
-        $scope.seat = $scope.seats[0];
-        $scope.loadCandidates();
+        $scope.loadItems('/years/' + $scope.year + '/' + $scope.district, 'seats', function() {
+            $scope.seat = $scope.seats[0];
+            $scope.loadCandidates();
+        });
     }
 
     /**
      * Load candidates
      */
     $scope.loadCandidates = function() {
-        $scope.loadItems('/years/' + $scope.year + '/' + $scope.district + '/' + $scope.seat, 'candidates');
+        $scope.loadItems('/years/' + $scope.year + '/' + $scope.district + '/' + $scope.seat, 'candidates', function() {
+            // noop
+        });
+    }
+
+    /**
+     * If you know the year in advance, district and seat, load it
+     *  up and display it. This doesn't do any error checking if
+     *  you call some non-existent combo.
+     */
+    $scope.goToSeat = function(year, district, seat) {
+        $scope.year     = year;
+        $scope.district = district;
+        $scope.seat     = seat;
+        $scope.loadCandidates();
     }
 
     /**
      * Generic method for calling a URI which returns a json array, and placing it
      * into an array by the given name on the scope
      */
-    $scope.loadItems = function(uri, name) {
+    $scope.loadItems = function(uri, name, cb) {
         $scope[name].length = 0;
         $scope.get(uri, function(items) {
           for(var i in items)
                 $scope[name].push(items[i]);
+          if(cb) cb();
         });
     }
 
@@ -71,16 +93,13 @@ function MainCtrl($scope) {
      * @TODO: Move this into an angular service
      */
     $scope.get = function(uri, func) {
-        $.ajax({
-            type: 'GET',
-            url: uri,
-            dataType: 'json',
-            success: func,
-            data: {},
-            async: false
-        });
+        $http.get(uri)
+            .success(func)
+            .error(function() {
+                alert('There was a server error loading the data');
+            });
     }
 
     /* Go */
     $scope.init();
-};
+});
